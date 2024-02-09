7fc365592764b809cdfe35bab4b6d692fcb7483a
Bryan Haskin <bhhaskin@bitsofsimplicity.com>
2024-02-09 12:08:13 -0800

    added input

---
+++ src/atoms/Input.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Input.module.scss';
+
+export default function Input(props) {
+  const {
+    className,
+    placeholder,
+    type,
+    onChange,
+    ...childrenProps
+  } = props;
+
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    onChange(e);
+  };
+
+  const handleFocus = () => {
+    setError(false);
+  };
+
+  const handleBlur = (e) => {
+    if (!e.target.validity.valid) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
+  const containerClassNames = () => {
+    const classNameArray = [styles.input, className];
+
+    if (error) {
+      classNameArray.push(styles['--error']);
+    }
+
+    return classNameArray.join(' ').trim();
+  };
+
+  return (
+    <div className={containerClassNames()}>
+      <input
+        {...childrenProps}
+        className={styles.input__control}
+        placeholder={placeholder}
+        type={type}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+      />
+    </div>
+  );
+}
+Input.propTypes = {
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+};
+
+Input.defaultProps = {
+  className: null,
+  // eslint-disable-next-line no-unused-vars
+  onChange: (e) => {},
+  placeholder: null,
+  type: 'text',
+};
--- src/stories/atoms/Input.stories.jsx
+++ src/stories/atoms/Input.stories.jsx
@@ -1,24 +1,23 @@
 /* eslint-disable react/function-component-definition */
-import Nav from '../../../components/blocks/Nav';
+import Input from '../../atoms/Input';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
-  title: 'Components/Blocks/Nav',
-  component: Nav,
+  title: 'Atoms/Input',
+  component: Input,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+    layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    children: { control: 'text' },
+  },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Default = (args) => <Nav {...args} />;
-
+export const Default = (args) => <Input {...args} />;
 Default.args = {
-  brand: {
-    label: 'B R A N D',
-    href: '#',
-  },
 };
